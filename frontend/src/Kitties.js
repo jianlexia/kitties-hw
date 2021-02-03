6ef7a4b3a35a829e1072183cf101a7e6f49436dc
jianlexia <56173042@qq.com>
2021-02-03 15:52:56 +0800

    home work

--- frontend/src/Kitties.js
+++ frontend/src/Kitties.js
@@ -6,7 +6,7 @@ import { TxButton } from './substrate-lib/components';
 
 import KittyCards from './KittyCards';
 
-export default function Kitties (props) {
+export default function Kitties(props) {
   const { api, keyring } = useSubstrate();
   const { accountPair } = props;
 
@@ -19,23 +19,76 @@ export default function Kitties (props) {
 
   const fetchKittyCnt = () => {
     /* TODO: 加代码，从 substrate 端读取数据过来 */
+    api.query.kittiesModule.kittiesCount(amount => {
+      //获取猫咪总数/ID
+      let kittyCount = amount.toJSON();
+      setKittyCnt(kittyCount);
+
+    })
   };
 
+  const fetchKittiesOwner = () => {
+    //获取猫咪的主人
+    api.query.kittiesModule.kittyOwners.multi([...Array(kittyCnt).keys()], (data) => {
+      let tempData = [];
+      data.map(row => {
+        if (row.isNone) {
+          tempData.push('猫不存在');
+        } else {
+          //Option 转字符串
+          //hash.toJSON() 或 hash.toHuman() 
+          //value.toJSON() 或 value.toHuman()
+          let kittyOwner = row.value.toHuman();
+
+          tempData.push(kittyOwner);
+        }
+      })
+      setKittyOwners(tempData);
+    })
+   // debugger;
+  }
+
   const fetchKitties = () => {
-    /* TODO: 加代码，从 substrate 端读取数据过来 */
-  };
+
+    api.query.kittiesModule.kitties.multi([...Array(kittyCnt).keys()], (data) => {
+      let tempData = [];
+      data.map(row => {
+        if (row.isNone) {
+          tempData.push('no kitty');
+        } else {
+          let kittyDna = row.value.toHuman();
+          console.log("dna == " + kittyDna);
+          tempData.push(kittyDna);
+        }
+      })
+      setKittyDNAs(tempData);
+ 
+    })
+  }
 
   const populateKitties = () => {
     /* TODO: 加代码，从 substrate 端读取数据过来 */
+    let kittiesAllInfo = [];
+
+    for (let idx = 0; idx < kittyCnt; idx++) {
+      kittiesAllInfo.push({
+        id: idx,
+        dna: kittyDNAs[idx],
+        owner: kittyOwners[idx]
+      })
+    }
+
+    setKitties(kittiesAllInfo);
   };
 
   useEffect(fetchKittyCnt, [api, keyring]);
-  useEffect(fetchKitties, [api, kittyCnt]);
+  useEffect(fetchKitties, [api, kittyDNAs, kittyOwners]);
+  useEffect(fetchKittiesOwner, [api, kittyDNAs, kittyOwners]);
   useEffect(populateKitties, [kittyDNAs, kittyOwners]);
 
   return <Grid.Column width={16}>
     <h1>小毛孩</h1>
-    <KittyCards kitties={kitties} accountPair={accountPair} setStatus={setStatus}/>
+    <KittyCards kitties={kitties} accountPair={accountPair} setStatus={setStatus} />
     <Form style={{ margin: '1em 0' }}>
       <Form.Field style={{ textAlign: 'center' }}>
         <TxButton
