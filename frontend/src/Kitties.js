9abc36c27a3d3b5e2eb2fba0f5dac2c00c4f2d6d
jianlexia <56173042@qq.com>
2021-02-03 16:31:49 +0800

    new branch

--- frontend/src/Kitties.js
+++ frontend/src/Kitties.js
@@ -18,7 +18,7 @@ export default function Kitties(props) {
   const [status, setStatus] = useState('');
 
   const fetchKittyCnt = () => {
-    /* TODO: 加代码，从 substrate 端读取数据过来 */
+    /* TODO: 加代码，从 substrate 端读取数据过来1 */
     api.query.kittiesModule.kittiesCount(amount => {
       //获取猫咪总数/ID
       let kittyCount = amount.toJSON();
